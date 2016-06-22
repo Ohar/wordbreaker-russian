21ec122a410bc931657d1be4da24bfb77fc9847a
Pavel lysenko <p.lysenko@zmeke.com>
2016-06-22 15:30:12 +0300

    Add test for CONSONANTS

---
+++ test/consts/consonants.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const chai       = require('chai'),
+      CONSONANTS = require('./../../consts/consonants');
+
+describe(
+  'CONSONANTS', () => {
+    it(
+      'Возвращает массив', () => {
+        chai.assert.isArray(CONSONANTS);
+      }
+    );
+
+    it(
+      'Возвращает массив строк', () => {
+        chai.assert.isString(CONSONANTS[0]);
+      }
+    );
+
+    it(
+      'Все строки содержат 1 букву', () => {
+        CONSONANTS.forEach(consonant => {
+          chai.assert(consonant.length === 1);
+        })
+      }
+    );
+  }
+);
