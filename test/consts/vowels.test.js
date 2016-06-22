61f75b73b6cf3a9d90b30ef75b7ad72f15a44e93
Pavel lysenko <p.lysenko@zmeke.com>
2016-06-22 15:46:37 +0300

    Add test for VOWELS

---
+++ test/consts/vowels.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const chai   = require('chai'),
+      VOWELS = require('./../../consts/vowels');
+
+describe(
+  'VOWELS', () => {
+    it(
+      'Возвращает массив', () => {
+        chai.assert.isArray(VOWELS);
+      }
+    );
+
+    it(
+      'Возвращает массив строк', () => {
+        chai.assert.isString(VOWELS[0]);
+      }
+    );
+
+    it(
+      'Все строки содержат 1 букву', () => {
+        VOWELS.forEach(
+          consonant => {
+            chai.assert(consonant.length === 1);
+          }
+        )
+      }
+    );
+  }
+);
