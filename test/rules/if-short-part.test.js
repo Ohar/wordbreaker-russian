46b07640e62229914878db5ef83abf4a787aeb80
Pavel lysenko <p.lysenko@zmeke.com>
2016-10-07 12:57:47 +0300

    Test ifShortPart

---
+++ test/rules/if-short-part.test.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const chai        = require('chai'),
+      ifShortPart = require('./../../rules/if-short-part');
+
+describe(
+  'ifShortPart', () => {
+
+    it(
+      'Это функция', () => {
+        chai.assert.isFunction(ifShortPart);
+      }
+    );
+
+    it(
+      'Правильно работает', () => {
+        const inputs = [
+          {
+            position: 0,
+            word    : 'акация',
+            result  : true,
+          },
+          {
+            position: 1,
+            word    : 'акация',
+            result  : false,
+          },
+          {
+            position: 2,
+            word    : 'акация',
+            result  : false,
+          },
+          {
+            position: 3,
+            word    : 'акация',
+            result  : false,
+          },
+          {
+            position: 4,
+            word    : 'акация',
+            result  : true,
+          },
+        ];
+
+        inputs.forEach(
+          input => {
+            chai.assert.equal(
+              ifShortPart(
+                input.position,
+                input.word.split('')
+              ),
+              input.result
+            );
+          }
+        );
+      }
+    );
+
+  }
+);
