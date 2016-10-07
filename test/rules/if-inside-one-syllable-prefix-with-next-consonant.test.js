e7220f6bfecc6933a39dc57286d086301be6b424
Pavel lysenko <p.lysenko@zmeke.com>
2016-10-07 12:41:54 +0300

    ifInsideOneSyllablePrefixWithNextConsonant: test of correctness

--- test/rules/if-inside-one-syllable-prefix-with-next-consonant.test.js
+++ test/rules/if-inside-one-syllable-prefix-with-next-consonant.test.js
@@ -5,10 +5,86 @@ const chai                                       = require('chai'),
 
 describe(
   'ifInsideOneSyllablePrefixWithNextConsonant', () => {
+
     it(
       'Это функция', () => {
         chai.assert.isFunction(ifInsideOneSyllablePrefixWithNextConsonant);
       }
     );
+
+    it(
+      'Правильно работает', () => {
+        const inputs = [
+          {
+            position: 0,
+            word: 'подбить',
+            result: true,
+          },
+          {
+            position: 1,
+            word: 'подбить',
+            result: true,
+          },
+          {
+            position: 2,
+            word: 'подбить',
+            result: false,
+          },
+          {
+            position: 3,
+            word: 'подбить',
+            result: false,
+          },
+          {
+            position: 4,
+            word: 'подбить',
+            result: false,
+          },
+          {
+            position: 5,
+            word: 'подбить',
+            result: false,
+          },
+          {
+            position: 0,
+            word: 'размах',
+            result: true,
+          },
+          {
+            position: 1,
+            word: 'размах',
+            result: true,
+          },
+          {
+            position: 2,
+            word: 'размах',
+            result: false,
+          },
+          {
+            position: 3,
+            word: 'размах',
+            result: false,
+          },
+          {
+            position: 4,
+            word: 'размах',
+            result: false,
+          },
+        ];
+
+        inputs.forEach(
+          input => {
+            chai.assert.equal(
+              ifInsideOneSyllablePrefixWithNextConsonant(
+                input.position,
+                input.word.split('')
+              ),
+              input.result
+            );
+          }
+        );
+      }
+    );
+
   }
 );
