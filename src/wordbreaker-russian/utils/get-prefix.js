47dbf9e807555be1efc0ba960f5a6b94f7aa04d3
Pavel Lysenko <code@ohar.name>
2019-01-18 17:41:36 +0300

    getPrefix: fix code style

--- src/wordbreaker-russian/utils/get-prefix.js
+++ src/wordbreaker-russian/utils/get-prefix.js
@@ -1,21 +1,20 @@
 import ifStartWith from './../utils/if-start-with'
-import PREFIXES    from './../consts/prefixes'
+import PREFIXES from './../consts/prefixes'
 
 export default function getPrefix (word) {
-  let prefix     = ''
+    let prefix     = ''
+    let prefixTemp = ''
 
-let prefixTemp = ''
+    PREFIXES.forEach(
+        el => {
+            if (
+                ifStartWith(word, el) &&
+                prefixTemp.length < el.length
+            ) {
+                prefix = prefixTemp = el
+            }
+        },
+    )
 
-  PREFIXES.forEach(
-      el => {
-          if (
-                 ifStartWith(word, el) &&
-              prefixTemp.length < el.length
-          ) {
-              prefix = prefixTemp = el
-          }
-      }
-  )
-
-  return prefix
+    return prefix
 }
