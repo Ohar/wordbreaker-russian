4df868b5118899ea58f34f215fa84a0f798bd83b
Pavel Lysenko <code@ohar.name>
2019-01-18 17:38:07 +0300

    isDoubleConsonantWithVowels: handle previous letters

--- src/wordbreaker-russian/rules/is-double-consonant-with-vowels.js
+++ src/wordbreaker-russian/rules/is-double-consonant-with-vowels.js
@@ -7,13 +7,26 @@ import isConsonant from './../utils/is-consonant'
 import isVowel from './../utils/is-vowel'
 
 export default function isDoubleConsonantWithVowels (pos, arr) {
-    const cur            = getLetter(pos, arr)
+    const beforePrev     = getLetter(pos - 2, arr)
+    const prev           = getLetter(pos - 1, arr)
+    const curr           = getLetter(pos + 0, arr)
     const next           = getLetter(pos + 1, arr)
     const afterNext      = getLetter(pos + 2, arr)
     const afterAfterNext = getLetter(pos + 3, arr)
 
-    return next === afterNext &&
-           isConsonant(next) &&
-           isVowel(cur) &&
-           isVowel(afterAfterNext)
+    const isDoubleConsonantsBefore = Boolean(
+        curr === prev &&
+        isConsonant(curr) &&
+        isVowel(beforePrev) &&
+        isVowel(next)
+    )
+
+    const isDoubleConsonantsAfter = Boolean(
+        next === afterNext &&
+        isConsonant(next) &&
+        isVowel(curr) &&
+        isVowel(afterAfterNext)
+    )
+
+    return isDoubleConsonantsBefore || isDoubleConsonantsAfter
 }
