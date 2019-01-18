3694ff892f23aed3a4f4f5c52171086a54518c0d
Pavel Lysenko <code@ohar.name>
2019-01-18 16:44:01 +0300

    Test, ifVowelsBeforeAndAfter: fix code style; #7

--- test/rules/if-vowels-before-and-after.test.js
+++ test/rules/if-vowels-before-and-after.test.js
@@ -7,102 +7,90 @@ import ifVowelsBeforeAndAfter from '@/wordbreaker-russian/rules/if-vowels-before
 describe(
     'ifVowelsBeforeAndAfter', () => {
         it(
-            'Это функция', () => {
-                chai.assert.isFunction(ifVowelsBeforeAndAfter)
-            }
+            'Это функция',
+            () => chai.assert.isFunction(ifVowelsBeforeAndAfter)
         )
 
         describe(
-            'Правильно работает', () => {
-                {
-                    const word = 'просмотр'
+            'Правильно работает',
+            () => {
+                const testCaseArr = [
+                    {
+                        inputWord: 'просмотр',
+                        expectedOutputData: [
+                            {
+                                position: 0,
+                                result: true,
+                            },
+                            {
+                                position: 1,
+                                result: true,
+                            },
+                            {
+                                position: 2,
+                                result: false,
+                            },
+                            {
+                                position: 3,
+                                result: true,
+                            },
+                            {
+                                position: 4,
+                                result: true,
+                            },
+                            {
+                                position: 5,
+                                result: true,
+                            },
+                            {
+                                position: 6,
+                                result: true,
+                            },
+                        ],
+                    },
+                    {
+                        inputWord: 'страх',
+                        expectedOutputData: [
+                            {
+                                position: 0,
+                                result: true,
+                            },
+                            {
+                                position: 1,
+                                result: true,
+                            },
+                            {
+                                position: 2,
+                                result: true,
+                            },
+                            {
+                                position: 3,
+                                result: true,
+                            },
+                        ],
+                    },
+                ]
 
-                    it(
-                        word, () => {
-                            const inputs = [
-                                {
-                                    position: 0,
-                                    result: true,
-                                },
-                                {
-                                    position: 1,
-                                    result: true,
-                                },
-                                {
-                                    position: 2,
-                                    result: false,
-                                },
-                                {
-                                    position: 3,
-                                    result: true,
-                                },
-                                {
-                                    position: 4,
-                                    result: true,
-                                },
-                                {
-                                    position: 5,
-                                    result: true,
-                                },
-                                {
-                                    position: 6,
-                                    result: true,
-                                },
-                            ]
-
-                            inputs.forEach(
-                                input => {
-                                    chai.assert.equal(
-                                        ifVowelsBeforeAndAfter(
-                                            input.position,
-                                            word
-                                        ),
-                                        input.result
-                                    )
-                                }
-                            )
-                        }
-                    )
-                }
-
-                {
-                    const word = 'страх'
-
-                    it(
-                        word, () => {
-                            const inputs = [
-                                {
-                                    position: 0,
-                                    result: true,
-                                },
-                                {
-                                    position: 1,
-                                    result: true,
-                                },
-                                {
-                                    position: 2,
-                                    result: true,
-                                },
-                                {
-                                    position: 3,
-                                    result: true,
-                                },
-                            ]
-
-                            inputs.forEach(
-                                input => {
-                                    chai.assert.equal(
-                                        ifVowelsBeforeAndAfter(
-                                            input.position,
-                                            word
-                                        ),
-                                        input.result
-                                    )
-                                }
-                            )
-                        }
-                    )
-                }
+                testCaseArr.forEach(
+                    ({inputWord, expectedOutputData}) => {
+                        it(
+                            inputWord,
+                            () => {
+                                expectedOutputData.forEach(
+                                    ({position, result}) => {
+                                        chai.assert.equal(
+                                            ifVowelsBeforeAndAfter(
+                                                position,
+                                                inputWord
+                                            ),
+                                            result
+                                        )
+                                    }
+                                )
+                            }
+                        )
+                    }
+                )
             }
         )
     }
